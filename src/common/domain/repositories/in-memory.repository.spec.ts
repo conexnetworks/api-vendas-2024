7e7d7c05ad6aed33f17369299ed1209954bbccb4
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-08-31 15:59:39 -0300

    Tests InMemoryRepository - findById method

--- src/common/domain/repositories/in-memory.repository.spec.ts
+++ src/common/domain/repositories/in-memory.repository.spec.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from 'crypto'
 import { InMemoryRepository } from './in-memory.repository'
+import { NotFoundError } from '../errors/not-found-error'
 
 type StubModelProps = {
   id: string
@@ -57,5 +58,21 @@ describe('InMemoryRepository unit tests', () => {
   it('should inserts a new model', async () => {
     const result = await sut.insert(model)
     expect(result).toStrictEqual(sut.items[0])
+  })
+
+  it('should throw error when id not found', async () => {
+    await expect(sut.findById('fake_id')).rejects.toThrow(
+      new NotFoundError('Model not found using ID fake_id'),
+    )
+    const id = randomUUID()
+    await expect(sut.findById(id)).rejects.toThrow(
+      new NotFoundError(`Model not found using ID ${id}`),
+    )
+  })
+
+  it('should find a model by id', async () => {
+    const data = await sut.insert(model)
+    const result = await sut.findById(data.id)
+    expect(result).toStrictEqual(data)
   })
 })
