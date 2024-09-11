92551a4390235d42b99ecf39977352bf2892be35
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-09-11 19:12:45 -0300

    ProductsTypeormRepository - findById

--- src/products/infrastructure/typeorm/repositories/products-typeorm.repository.ts
+++ src/products/infrastructure/typeorm/repositories/products-typeorm.repository.ts
@@ -11,6 +11,7 @@ import {
 import { Repository } from 'typeorm'
 import { Product } from '../entities/products.entity'
 import { dataSource } from '@/common/infrastructure/typeorm'
+import { NotFoundError } from '@/common/domain/errors/not-found-error'
 
 export class ProductsTypeormRepository implements ProductsRepository {
   sortableFields: string[] = ['name', 'created_at']
@@ -40,8 +41,8 @@ export class ProductsTypeormRepository implements ProductsRepository {
     throw new Error('Method not implemented.')
   }
 
-  findById(id: string): Promise<ProductModel> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<ProductModel> {
+    return this._get(id)
   }
 
   update(model: ProductModel): Promise<ProductModel> {
@@ -54,5 +55,13 @@ export class ProductsTypeormRepository implements ProductsRepository {
 
   search(props: SearchInput): Promise<SearchOutput<ProductModel>> {
     throw new Error('Method not implemented.')
+  }
+
+  protected async _get(id: string): Promise<ProductModel> {
+    const product = await this.productsRepository.findOneBy({ id })
+    if (!product) {
+      throw new NotFoundError(`Product not found using ID ${id}`)
+    }
+    return product
   }
 }
