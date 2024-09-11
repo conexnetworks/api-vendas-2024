727c51cf7de3606f535348acdb7561021b9369ac
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-09-11 20:32:25 -0300

    ProductsTypeormRepository Integration Tests - findById

--- src/products/infrastructure/typeorm/repositories/products-typeorm.repository.int-spec.ts
+++ src/products/infrastructure/typeorm/repositories/products-typeorm.repository.int-spec.ts
@@ -1,11 +1,43 @@
+import { testDataSource } from '@/common/infrastructure/typeorm/testing/data-source'
 import { ProductsTypeormRepository } from './products-typeorm.repository'
+import { Product } from '../entities/products.entity'
+import { NotFoundError } from '@/common/domain/errors/not-found-error'
+import { randomUUID } from 'crypto'
+import { ProductsDataBuilder } from '../../testing/helpers/products-data-builder'
 
 describe('ProductsTypeormRepository integration tests', () => {
-  let productsRepository: ProductsTypeormRepository
+  let ormRepository: ProductsTypeormRepository
 
-  beforeEach(() => {})
+  beforeAll(async () => {
+    await testDataSource.initialize()
+  })
 
-  describe('method', () => {
-    it('testing', () => {})
+  afterAll(async () => {
+    await testDataSource.destroy()
+  })
+
+  beforeEach(async () => {
+    await testDataSource.manager.query('DELETE FROM products')
+    ormRepository = new ProductsTypeormRepository()
+    ormRepository.productsRepository = testDataSource.getRepository(Product)
+  })
+
+  describe('findById', () => {
+    it('should generate an error when the product is not found', async () => {
+      const id = randomUUID()
+      await expect(ormRepository.findById(id)).rejects.toThrow(
+        new NotFoundError(`Product not found using ID ${id}`),
+      )
+    })
+
+    it('should finds a product by id', async () => {
+      const data = ProductsDataBuilder({})
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      const result = await ormRepository.findById(product.id)
+      expect(result.id).toEqual(product.id)
+      expect(result.name).toEqual(product.name)
+    })
   })
 })
