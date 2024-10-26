b2ecba2e97e14629b83bfcf0ac4ffbb699531240
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-10-26 10:12:45 -0300

    Criação do DTO ProductOutput

--- src/products/application/usecases/get-product.usecase.ts
+++ src/products/application/usecases/get-product.usecase.ts
@@ -1,19 +1,13 @@
 import { ProductsRepository } from '@/products/domain/repositories/products.repository'
 import { inject, injectable } from 'tsyringe'
+import { ProductOutput } from '../dtos/product-output.dto'
 
 export namespace GetProductUseCase {
   export type Input = {
     id: string
   }
 
-  export type Output = {
-    id: string
-    name: string
-    price: number
-    quantity: number
-    created_at: Date
-    updated_at: Date
-  }
+  export type Output = ProductOutput
 
   @injectable()
   export class UseCase {
@@ -23,16 +17,11 @@ export namespace GetProductUseCase {
     ) {}
 
     async execute(input: Input): Promise<Output> {
-      const product = await this.productsRepository.findById(input.id)
+      const product: ProductOutput = await this.productsRepository.findById(
+        input.id,
+      )
 
-      return {
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        quantity: product.quantity,
-        created_at: product.created_at,
-        updated_at: product.updated_at,
-      }
+      return product
     }
   }
 }
