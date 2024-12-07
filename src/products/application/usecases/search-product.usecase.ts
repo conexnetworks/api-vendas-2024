5e6c51f28e11b5c8fbc6ba04730c6d94ca0b37c5
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-12-07 11:57:43 -0300

    Criação do caso de uso SearchProductUseCase - pt. 2

--- src/products/application/usecases/search-product.usecase.ts
+++ src/products/application/usecases/search-product.usecase.ts
@@ -1,11 +1,16 @@
 import { ProductsRepository } from '@/products/domain/repositories/products.repository'
 import { inject, injectable } from 'tsyringe'
 import { SearchInputDto } from '../dtos/search-input.dto'
+import {
+  PaginationOutputDto,
+  PaginationOutputMapper,
+} from '../dtos/pagination-output.dto'
+import { ProductModel } from '@/products/domain/models/products.model'
 
 export namespace SearchProductUseCase {
   export type Input = SearchInputDto
 
-  export type Output = void
+  export type Output = PaginationOutputDto<ProductModel>
 
   @injectable()
   export class UseCase {
@@ -16,7 +21,7 @@ export namespace SearchProductUseCase {
 
     async execute(input: Input): Promise<Output> {
       const searchResult = await this.productsRepository.search(input)
-      return
+      return PaginationOutputMapper.toOutput(searchResult.items, searchResult)
     }
   }
 }
