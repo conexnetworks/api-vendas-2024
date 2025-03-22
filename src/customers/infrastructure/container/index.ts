29deb7fe8ebdedc5e4f80a090724587d175f9bf1
Aluizio Developer <aluiziodeveloper@gmail.com>
2025-03-22 11:39:08 -0300

    Criação do caso de uso CreateCustomerUseCase

--- src/customers/infrastructure/container/index.ts
+++ src/customers/infrastructure/container/index.ts
@@ -2,9 +2,14 @@ import { dataSource } from '@/common/infrastructure/typeorm'
 import { CustomersTypeormRepository } from '@/customers/infrastructure/typeorm/repositories/customers-typeorm.repository'
 import { container } from 'tsyringe'
 import { Customer } from '../typeorm/entities/customers.entity'
+import { CreateCustomerUseCase } from '@/customers/application/usecases/create-customer.usecase'
 
 container.registerSingleton('CustomersRepository', CustomersTypeormRepository)
 container.registerInstance(
   'CustomersDefaultRepositoryTypeorm',
   dataSource.getRepository(Customer),
+)
+container.registerSingleton(
+  'CreateCustomerUseCase',
+  CreateCustomerUseCase.UseCase,
 )
